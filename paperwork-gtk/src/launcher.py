9648ae706be98c05003553f5995e3bbd54159e6d
Jerome Flesch <jflesch@openpaper.work>
2019-07-26 18:31:59 +0200

    GTK: launcher: really fix it when frozen ...
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- paperwork-gtk/src/launcher.py
+++ paperwork-gtk/src/launcher.py
@@ -24,14 +24,12 @@ import sys
 
 if __name__ == "__main__":
     if "python" not in sys.executable or '__file__' not in globals():
-        filepath = sys.executable
+        data_base_path = os.path.dirname(os.path.realpath(sys.executable))
     else:
-        filepath = __file__
-    if filepath is not None:
         data_base_path = os.path.join(
             os.path.dirname(os.path.realpath(__file__)), ".."
         )
-        os.chdir(data_base_path)
+    os.chdir(data_base_path)
 
     from paperwork.paperwork import main
     main()
