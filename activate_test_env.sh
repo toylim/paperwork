adfe4751a36432efb65127d66874a1e1509d9677
Jerome Flesch <jflesch@openpaper.work>
2019-08-17 13:42:54 +0200

    activate_test_env.sh: don't continue if a step has failed
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- activate_test_env.sh
+++ activate_test_env.sh
@@ -1,3 +1,5 @@
-make venv
-source venv/bin/activate
-cd sub/libinsane && source ./activate_test_env.sh ; cd ../..
+make venv && \
+source venv/bin/activate && \
+cd sub/libinsane && \
+source ./activate_test_env.sh && \
+cd ../..
