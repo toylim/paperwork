f60ffb267d16e344ee918bd585ef587760982a18
Jerome Flesch <jflesch@openpaper.work>
2019-09-20 08:48:58 +0200

    Add 'paperwork-json' and 'paperwork-cli': command line frontends for Paperwork (no command implemented yet)
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

---
+++ paperwork-backend/src/paperwork_backend/__init__.py
@@ -0,0 +1,36 @@
+DEFAULT_CONFIG_PLUGIN = 'paperwork_backend.config.file'
+
+DEFAULT_PLUGINS = [
+    'openpaperwork_core.config_file',
+    'paperwork_backend.docexport.img',
+    'paperwork_backend.docexport.pdf',
+    'paperwork_backend.docexport.pillowfight',
+    'paperwork_backend.docimport.img',
+    'paperwork_backend.docimport.pdf',
+    'paperwork_backend.docscan.libinsane',
+    'paperwork_backend.docscan.scan2doc',
+    'paperwork_backend.fs.gio',
+    'paperwork_backend.fs.memory',
+    'paperwork_backend.guesswork.label_guesser',
+    'paperwork_backend.index.whoosh',
+    'paperwork_backend.model.hocr',
+    'paperwork_backend.model.img',
+    'paperwork_backend.model.labels',
+    'paperwork_backend.model.pdf',
+    'paperwork_backend.model.workdir',
+    'paperwork_backend.ocr.pyocr',
+    'paperwork_backend.pillow.img',
+    'paperwork_backend.pillow.pdf',
+]
+
+DEFAULT_GUI_PLUGINS = [
+    'openpaperwork_core.log_collector',
+    'paperwork_backend.beacon.stats',
+    'paperwork_backend.beacon.sysinfo',
+    'paperwork_backend.beacon.update',
+] + DEFAULT_PLUGINS
+
+DEFAULT_SHELL_PLUGINS = [
+    'openpaperwork_core.log_print',
+    "openpaperwork_core.mainloop_asyncio",
+] + DEFAULT_PLUGINS
