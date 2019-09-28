8f845c0a1cb983c33835e9a5e10552fb1bb94b15
Jerome Flesch <jflesch@openpaper.work>
2019-09-28 17:08:40 +0200

    Core: log_print: color warnings and error logs
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- openpaperwork-core/src/openpaperwork_core/log_print.py
+++ openpaperwork-core/src/openpaperwork_core/log_print.py
@@ -4,6 +4,7 @@ Minimalist log handling. Print everything to stdout by default
 
 import datetime
 import logging
+import os
 import sys
 
 from . import PluginBase
@@ -11,19 +12,56 @@ from . import PluginBase
 
 LOGGER = logging.getLogger(__name__)
 
+LOG_FORMAT = '[%(levelname)-6s] [%(name)-30s] %(message)s\n'
+
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;{}m"
+BOLD_SEQ = "\033[1m"
+
+COLORS = {
+    'CRITICAL': 31,
+    'ERROR': 31,
+    'WARNING': 33,
+}
+
+
+class ColorFormatter(logging.Formatter):
+    def format(self, record):
+        levelname = record.levelname
+        if levelname in COLORS:
+            levelname = (
+                COLOR_SEQ.format(COLORS[levelname])
+                + levelname + RESET_SEQ
+            )
+            record.levelname = levelname
+        return logging.Formatter.format(self, record)
+
 
 class LogHandler(logging.Handler):
-    LOG_FORMAT = '[%(levelname)-6s] [%(name)-30s] %(message)s\n'
 
     def __init__(self):
         super().__init__()
-        self.formatter = logging.Formatter(self.LOG_FORMAT)
-        self.out_fd = sys.stdout
+        self._set_out_fd(sys.stdout)
         sys.excepthook = self.on_uncatched_exception_cb
 
     def emit(self, record):
         line = self.formatter.format(record)
-        self.out_fd.write(line)
+        self._out_fd.write(line)
+
+    def _get_out_fd(self):
+        return self._out_fd
+
+    def _set_out_fd(self, fd):
+        isatty = False
+        if hasattr(fd, 'fileno'):
+            isatty = os.isatty(fd.fileno())
+        if isatty:
+            self.formatter = ColorFormatter(LOG_FORMAT)
+        else:
+            self.formatter = logging.Formatter(LOG_FORMAT)
+        self._out_fd = fd
+
+    out_fd = property(_get_out_fd, _set_out_fd)
 
     def on_uncatched_exception_cb(self, exc_type, exc_value, exc_tb):
         LOGGER.error(
