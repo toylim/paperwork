fb6eeb24b4a31f2260d8fa60ece9aceeba8a5f29
Jerome Flesch <jflesch@openpaper.work>
2020-01-02 21:46:31 +0100

    set_apt_proxy.sh: fix generated APT config
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- set_apt_proxy.sh
+++ set_apt_proxy.sh
@@ -10,7 +10,7 @@ echo "Tags: ${CI_RUNNER_TAGS}"
 
 if echo "${CI_RUNNER_TAGS}" | grep set_apt_proxy > /dev/null ; then
 	echo "Proxy ${APT_PROXY_CACHE_HOST}:${APT_PROXY_CACHE_PORT}"
-	echo "Acquire::http { Proxy \"http://${APT_PROXY_CACHE}:${APT_PROXY_PORT}\"; }" >> /etc/apt/apt.conf.d/proxy
+	echo "Acquire::http { Proxy \"http://${APT_PROXY_CACHE_HOST}:${APT_PROXY_CACHE_PORT}\"; }" >> /etc/apt/apt.conf.d/proxy
 	echo "APT has been configured to use this proxy"
 else
 	echo "No tag set_apt_proxy"
