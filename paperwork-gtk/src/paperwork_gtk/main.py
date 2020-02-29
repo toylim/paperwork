94afc8a1dee38713ed78d267a16d3b81931844a6
Jerome Flesch <jflesch@openpaper.work>
2020-02-29 16:37:55 +0100

    GTK: Drop progress bar at the bottom of the main window. Will be replaced by a Gnome progress indicator in the header bar
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- paperwork-gtk/src/paperwork_gtk/main.py
+++ paperwork-gtk/src/paperwork_gtk/main.py
@@ -43,7 +43,6 @@ DEFAULT_GUI_PLUGINS = paperwork_backend.DEFAULT_PLUGINS + [
     'paperwork_gtk.mainwindow.docview.pageview.boxes.search',
     'paperwork_gtk.mainwindow.docview.scanview',
     'paperwork_gtk.mainwindow.search.field',
-    'paperwork_gtk.mainwindow.statusbar',
     'paperwork_gtk.mainwindow.window',
     'paperwork_gtk.readonly',
     'paperwork_gtk.settings',
