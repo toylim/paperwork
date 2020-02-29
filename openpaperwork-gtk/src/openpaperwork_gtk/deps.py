4e380377a46d89dbc6761b8e736460c9a4df228b
Jerome Flesch <jflesch@openpaper.work>
2020-02-29 16:34:44 +0100

    Core-GTK: add pango to the possible deps
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- openpaperwork-gtk/src/openpaperwork_gtk/deps.py
+++ openpaperwork-gtk/src/openpaperwork_gtk/deps.py
@@ -14,3 +14,10 @@ GTK = {
     'suse': 'python-gtk',
     'ubuntu': 'gir1.2-gtk-3.0',
 }
+
+PANGO = {
+    'debian': 'gir1.2-pango-1.0',
+    'linuxmint': 'gir1.2-pango-1.0',
+    'raspbian': 'gir1.2-pango-1.0',
+    'ubuntu': 'gir1.2-pango-1.0',
+}
