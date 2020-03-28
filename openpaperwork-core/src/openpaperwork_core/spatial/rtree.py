7284125a8f6b029d804f3d87524b9a3ab8fb533f
Jerome Flesch <jflesch@openpaper.work>
2020-03-28 23:13:43 +0100

    Core: rtree: - Fix selection of leaf nodes (use expected enlargement instead of overlap) - Use Guttman Quadratic Split heuristic to split leaf nodes
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- openpaperwork-core/src/openpaperwork_core/spatial/rtree.py
+++ openpaperwork-core/src/openpaperwork_core/spatial/rtree.py
@@ -2,9 +2,6 @@
 Spatial indexer that works in a way very similar to R-tree. Except for the
 way the nodes are split because I'm a lazy bastard and it's good enough.
 """
-# TODO(Jflesch): balancing is just wrong. --> splitting should be done with
-# Guttman Quadratic or any better heuristic.
-
 import math
 import logging
 
@@ -12,11 +9,15 @@ import openpaperwork_core
 
 
 LOGGER = logging.getLogger(__name__)
-NB_CHILDREN_PER_NODES = 2
+NB_CHILDREN_PER_NODES = 4  # must be a multiple of 2 because I'm a lazy bastard
 
 # Node: R-tree page (leaf or not)
 # Non-leaf: children are other nodes
 # Leaf: last R-tree node: children are tuples (box, obj)
+
+
+def compute_area(box):
+    return abs((box[1][0] - box[0][0]) * (box[1][1] - box[0][1]))
 
 
 class Node(object):
@@ -30,76 +31,126 @@ class Node(object):
 
         self.parent = None
 
-    @staticmethod
-    def compute_area(box):
-        return abs((box[1][0] - box[0][0]) * (box[1][1] - box[0][1]))
-
-    def compute_overlap(self, other):
-        if self.box is None:
-            return 0
-
-        ours = self.box
-
-        area_intersect = (
-            (min(ours[1][0], other[1][0]) - max(ours[0][0], other[0][0]))
-            * (min(ours[1][1], other[1][1]) - max(ours[0][1], other[0][1]))
+    def compute_enlargment(self, box):
+        enlarged = (
+            (
+                min(self.box[0][0], box[0][0]),
+                min(self.box[0][1], box[0][1]),
+            ),
+            (
+                max(self.box[1][0], box[1][0]),
+                max(self.box[1][1], box[1][1]),
+            ),
         )
-        other_area = self.compute_area(other)
-        return self.area + other_area - area_intersect
+        enlarged = compute_area(enlarged)
+        return enlarged - self.area
 
     def set_box(self, box):
         self.box = box
         if box is None:  # root node
             self.area = math.inf
         else:
-            self.area = self.compute_area(box)
+            self.area = compute_area(box)
 
-    def recompute_box(self):
-        if self.leaf:
-            box = (
-                (
-                    min(x[0][0][0] for x in self.children),
-                    min(x[0][0][1] for x in self.children),
-                ),
-                (
-                    max(x[0][1][0] for x in self.children),
-                    max(x[0][1][1] for x in self.children),
-                ),
-            )
-        else:
-            box = (
-                (
-                    min(x.box[0][0] for x in self.children),
-                    min(x.box[0][1] for x in self.children),
-                ),
-                (
-                    max(x.box[1][0] for x in self.children),
-                    max(x.box[1][1] for x in self.children),
-                ),
-            )
-        self.set_box(box)
-        if self.parent is not None:
-            self.parent.recompute_box()
+    def recompute_box(self, recurse=True):
+        depth = 0
+        s = self
+        while True:
+            depth += 1
+            assert(depth < 512)
+            if s.leaf:
+                box = (
+                    (
+                        min(x[0][0][0] for x in s.children),
+                        min(x[0][0][1] for x in s.children),
+                    ),
+                    (
+                        max(x[0][1][0] for x in s.children),
+                        max(x[0][1][1] for x in s.children),
+                    ),
+                )
+            else:
+                box = (
+                    (
+                        min(x.box[0][0] for x in s.children),
+                        min(x.box[0][1] for x in s.children),
+                    ),
+                    (
+                        max(x.box[1][0] for x in s.children),
+                        max(x.box[1][1] for x in s.children),
+                    ),
+                )
+            s.set_box(box)
+            if not recurse or s.parent is None:
+                break
+            s = s.parent
 
     def is_full(self):
         assert(self.leaf)
         return len(self.children) >= NB_CHILDREN_PER_NODES
 
+    @staticmethod
+    def _pick_seed(boxes):
+        """
+        Returns a pair of nodes that would be the most wasteful.
+        (see Guttman Quadratic Split algorithm)
+        """
+
+        assert(len(boxes) >= 2)
+
+        if len(boxes) == 2:  # minor optim
+            return tuple(boxes)
+
+        max_waste = -1
+        r = None
+
+        for (a_idx, a) in enumerate(boxes):
+            for b in boxes[a_idx + 1:]:
+                new_rect = (
+                    (
+                        min(a[0][0][0], b[0][0][0]),
+                        min(a[0][0][1], b[0][0][1]),
+                    ),
+                    (
+                        max(a[0][1][0], b[0][1][0]),
+                        max(a[0][1][1], b[0][1][1]),
+                    )
+                )
+                new_area = (
+                    (new_rect[1][0] - new_rect[0][0]) *
+                    (new_rect[1][1] - new_rect[0][1])
+                )
+                waste = new_area - a[2] - b[2]
+                if waste > max_waste:
+                    r = (a, b)
+
+        assert(r is not None)
+        return r
+
     def split(self):
         assert(self.leaf)
 
-        # Node is full --> we need to split it
-        # We should go with Guttman Quadratic algorithm
-        # but for now I'm a lazy bastard
-        assert(NB_CHILDREN_PER_NODES == 2)
-        assert(len(self.children) == 2)
-        node_a = Node(self.children[0][0])
-        node_a.insert_box(self.children[0])
+        our_children = self.children
+        picked = []
+
+        while len(our_children) > 0:
+            p = self._pick_seed(our_children)
+            picked.append(p)
+            for c in p:
+                our_children.remove(c)
+
+        node_a = Node()
         node_a.parent = self
-        node_b = Node(self.children[1][0])
-        node_b.insert_box(self.children[1])
+        node_a.children = [b[0] for b in picked]
+        node_a.recompute_box(recurse=False)
+
+        node_b = Node()
         node_b.parent = self
+        node_b.children = [b[1] for b in picked]
+        node_b.recompute_box(recurse=False)
+
         self.children = [node_a, node_b]
+
         self.leaf = False
 
     def insert_box(self, box):
@@ -115,12 +166,15 @@ class RTreeSpatialIndexer(object):
     def __init__(self, boxes):
         self.root = Node()
 
+        boxes = list(boxes)
+        LOGGER.info("Loading %d boxes in rtree", len(boxes))
+
         for (pos, obj) in boxes:
             self.insert(pos, obj)
 
-        # self._print_stats()
+        # self.print_stats()
 
-    def _print_stats(self):
+    def print_stats(self):
         """
         Not used at the moment, but useful to make sure the tree looks OK when
         debugging.
@@ -147,22 +201,25 @@ class RTreeSpatialIndexer(object):
             nb_boxes, nb_nodes, max_depth
         )
 
+    def _choose_leaf(self, box_position, node):
+        # keep picking up the child node that overlap as much of
+        # box_position as possible
+        return min({
+            (c.compute_enlargment(box_position), c)
+            for c in node.children
+        })[1]
+
     def _find_leaf(self, box_position, root=None):
         if root is None:
             n = self.root
         else:
             n = root
         while not n.leaf:
-            # keep picking up the child node that overlap as much of
-            # box_position as possible
-            n = max({
-                (c.compute_overlap(box_position), c)
-                for c in n.children
-            })[1]
+            n = self._choose_leaf(box_position, n)
         return n
 
-    def insert(self, box_position, box):
-        new_child = (box_position, box)
+    def insert(self, box_position, obj):
+        new_child = (box_position, obj, compute_area(box_position))
         leaf = self._find_leaf(box_position)
         if leaf.is_full():
             leaf.split()
