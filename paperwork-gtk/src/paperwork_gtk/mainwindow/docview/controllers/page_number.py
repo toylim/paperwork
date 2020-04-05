a46fa06842660f685b10fe384b4d03960b4afe0d
Jerome Flesch <jflesch@openpaper.work>
2020-04-06 00:51:54 +0200

    GTK: Fix page number: when looking for the active page, prefer widget on which are currently (instead of the one with the closest center) and ignore invisible widgets

--- paperwork-gtk/src/paperwork_gtk/mainwindow/docview/controllers/page_number.py
+++ paperwork-gtk/src/paperwork_gtk/mainwindow/docview/controllers/page_number.py
@@ -9,7 +9,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 class PageNumberController(BaseDocViewController):
-    def _update_current_page(self):
+    def _get_closest_widget(self):
         vadj = self.plugin.scroll.get_vadjustment()
         view_width = self.plugin.scroll.get_allocated_width()
         view_height = self.plugin.scroll.get_allocated_height()
@@ -18,9 +18,26 @@ class PageNumberController(BaseDocViewController):
             vadj.get_value() + (view_height / 2)
         )
 
+        # look if the center is precisely on in a widget
+        for widget in self.plugin.page_layout.get_children():
+            if not widget.get_visible():
+                continue
+            alloc = widget.get_allocation()
+            if alloc.width < 10 or alloc.height < 10:
+                continue
+            if (alloc.x <= center[0] and alloc.y <= center[1] and
+                    alloc.x + alloc.width >= center[0] and
+                    alloc.y + alloc.height >= center[1]):
+                return widget
+
+        # else look for the closest widget
         min_dist = (99999999999999999, None)
         for widget in self.plugin.page_layout.get_children():
+            if not widget.get_visible():
+                continue
             alloc = widget.get_allocation()
+            if alloc.width < 10 or alloc.height < 10:
+                continue
             widget_center = (
                 (alloc.x + (alloc.width / 2)),
                 (alloc.y + (alloc.height / 2)),
@@ -30,11 +47,13 @@ class PageNumberController(BaseDocViewController):
             dist = (dist_w * dist_w) + (dist_h * dist_h)
             if dist < min_dist[0]:
                 min_dist = (dist, widget)
+        return min_dist[1]
 
-        if min_dist[1] is None:
+    def _update_current_page(self):
+        widget = self._get_closest_widget()
+        if widget is None:
             return
-
-        page = self.plugin.widget_to_page[min_dist[1]]
+        page = self.plugin.widget_to_page[widget]
         self.plugin.core.call_all("on_page_shown", page.page_idx)
 
     def on_layout_size_allocate(self, layout):
