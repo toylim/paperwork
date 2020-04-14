3b286ac04586ad4b757f0f4a4c7a32817d692d9d
Jerome Flesch <jflesch@openpaper.work>
2020-04-14 19:49:01 +0200

    GTK: settings.storage: Use a custom GtkButton+GtkFileChooserDialog instead of a GtkFileChooserWidget. When using a GtkFileChooserWidget + action=select-folder, when the user clicks on it, instead of immediately opening a dialog, it first open an annoying combobox menu with a predefined set of folders that are usually of no-use as Paperwork work directory. --> With a custom GtkButton, we can directly open the GtkFileChooserDialog.
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- paperwork-gtk/src/paperwork_gtk/settings/storage.py
+++ paperwork-gtk/src/paperwork_gtk/settings/storage.py
@@ -1,8 +1,17 @@
 import gettext
 import logging
 
+try:
+    import gi
+    gi.require_version('Gtk', '3.0')
+    from gi.repository import Gtk
+    GTK_AVAILABLE = True
+except (ImportError, ValueError):
+    GTK_AVAILABLE = False
+
 import openpaperwork_core
 import openpaperwork_core.deps
+import openpaperwork_gtk.deps
 
 
 _ = gettext.gettext
@@ -14,10 +23,13 @@ class Plugin(openpaperwork_core.PluginBase):
 
     def __init__(self):
         super().__init__()
+        self.windows = []
 
     def get_interfaces(self):
         return [
+            'chkdeps',
             'gtk_settings',
+            'gtk_window_listener',
         ]
 
     def get_deps(self):
@@ -44,26 +56,62 @@ class Plugin(openpaperwork_core.PluginBase):
         super().init(core)
         self.workdir = self.core.call_success("config_get", "workdir")
 
+    def chkdeps(self, out: dict):
+        if not GTK_AVAILABLE:
+            out['gtk'].update(openpaperwork_gtk.deps.GTK)
+
+    def on_gtk_window_opened(self, window):
+        self.windows.append(window)
+
+    def on_gtk_window_closed(self, window):
+        self.windows.remove(window)
+
     def complete_settings(self, global_widget_tree):
         widget_tree = self.core.call_success(
             "gtk_load_widget_tree", "paperwork_gtk.settings", "storage.glade"
         )
 
-        workdir_chooser = widget_tree.get_object("work_dir_chooser")
-        if (self.core.call_success("fs_exists", self.workdir) is not None and
-                self.core.call_success("fs_isdir", self.workdir) is not None):
-            workdir_chooser.set_uri(self.workdir)
-        workdir_chooser.connect("file-set", self._on_file_set)
+        workdir_button = widget_tree.get_object("work_dir_chooser_button")
+        basename = self.core.call_success("fs_basename", self.workdir)
+        workdir_button.set_label(basename)
+        workdir_button.connect("clicked", self._on_button_clicked, widget_tree)
 
         self.core.call_success(
             "add_setting_to_dialog", global_widget_tree, _("Storage"),
             [widget_tree.get_object("workdir")]
         )
 
-    def _on_file_set(self, file_chooser):
-        workdir = file_chooser.get_uri()
+    def _on_button_clicked(self, button, widget_tree):
+        dialog = Gtk.FileChooserDialog(
+            _("Work Directory"),
+            self.windows[-1],
+            Gtk.FileChooserAction.SELECT_FOLDER,
+            (
+                Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                Gtk.STOCK_OPEN, Gtk.ResponseType.ACCEPT
+            )
+        )
+        dialog.set_local_only(False)
+        dialog.connect("response", self._on_dialog_response, widget_tree)
+        dialog.show_all()
+
+    def _on_dialog_response(self, dialog, response_id, widget_tree):
+        if (response_id != Gtk.ResponseType.ACCEPT and
+                response_id != Gtk.ResponseType.OK and
+                response_id != Gtk.ResponseType.YES and
+                response_id != Gtk.ResponseType.APPLY):
+            LOGGER.info("User canceled (response_id=%d)", response_id)
+            dialog.destroy()
+            return
+
+        workdir = dialog.get_uri()
+        dialog.set_visible(False)
+
         LOGGER.info("Setting work directory to %s", workdir)
         self.core.call_all("config_put", "workdir", workdir)
+
+        basename = self.core.call_success("fs_basename", workdir)
+        widget_tree.get_object("work_dir_chooser_button").set_label(basename)
 
     def config_save(self):
         workdir = self.core.call_success("config_get", "workdir")
