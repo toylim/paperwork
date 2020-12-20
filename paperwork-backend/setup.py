a298458cff748873fc8a760f8086ecf645e8cfc6
Jerome Flesch <jflesch@openpaper.work>
2020-12-20 15:50:33 +0100

    Backend: beacon.sysinfo: report some extra infos This plugin is going to be reused by IronScanner. IronScanner currently collects a little bit more info than Paperwork.
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- paperwork-backend/setup.py
+++ paperwork-backend/setup.py
@@ -77,6 +77,7 @@ There is no GUI here. The GUI is
         "distro",
         "openpaperwork-core",
         "Pillow",
+        "psutil",
         "pycountry",
         "pyocr",
         "pypillowfight>=0.3.0",
