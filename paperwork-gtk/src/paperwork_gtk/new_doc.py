81527c6b2cb48167c2aef913d8d7b8f3d3089fe4
Jerome Flesch <jflesch@openpaper.work>
2021-03-03 22:42:00 +0100

    Bug report 170: When the work directory has been changed, we have to make sure to drop any reference to it so the user doesn't use it by accident.
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- paperwork-gtk/src/paperwork_gtk/new_doc.py
+++ paperwork-gtk/src/paperwork_gtk/new_doc.py
@@ -16,6 +16,17 @@ class Plugin(openpaperwork_core.PluginBase):
             },
         ]
 
+    def config_put(self, opt, value, *args, **kwargs):
+        if opt != "workdir":
+            return None
+        # Bug report 170: When the work directory has been changed,
+        # we have to make sure to drop any reference to it so the user doesn't
+        # use it by accident.
+        # (keep in mind that self.new_doc = (doc_id, doc_url), and doc_url
+        # includes the work directory path)
+        self.new_doc = None
+        return None
+
     def get_new_doc(self):
         if self.new_doc is not None:
             if self.core.call_success("is_doc", self.new_doc[1]) is None:
