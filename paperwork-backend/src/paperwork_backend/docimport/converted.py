81f4494292a392d49616b599d4b92b0bfdbfd32b
Jerome Flesch <jflesch@openpaper.work>
2021-05-24 20:47:39 +0200

    Backend: docimport.converted: Fix import of converted files (.docx, etc)
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- paperwork-backend/src/paperwork_backend/docimport/converted.py
+++ paperwork-backend/src/paperwork_backend/docimport/converted.py
@@ -79,6 +79,9 @@ class SingleDocImporterFactory(object):
         file_ext = file_url.rsplit(".", 1)[-1].lower()
         return file_ext in self.plugin.file_types_by_ext
 
+    def get_required_data(self, file_uri):
+        return set()
+
     def make_importer(self, file_import, file_uri, data):
         return SingleDocImporter(self.plugin, file_import, file_uri)
 
